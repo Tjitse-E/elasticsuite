b5293e94e135a08c3fb8c997a38aea48513a20ae
Romain Ruaud <romain.ruaud@smile.fr>
2018-10-23 17:19:22 +0200

    test

--- Resources/travis/before_install.sh
+++ Resources/travis/before_install.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
-set -e
-trap '>&2 echo Error: Command \`$BASH_COMMAND\` on line $LINENO failed with exit code $?' ERR
-echo '==> Disabling xdebug, adjusting memory limit to -1.'
-phpenv config-rm xdebug.ini
-echo 'memory_limit = -1' >> ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/travis.ini
-phpenv rehash;
+mkdir -p app/etc var
+
+# If no stability configured, do nothing and keep installing the given Magento2 version.
+if [[ -z "${MAGENTO_STABILITY}" ]]; then
+  VERSION=$MAGENTO_VERSION
+else
+  # Else, switch explicit version to closest one with ~
+  VERSION="~${MAGENTO_VERSION}"
+fi
+
+composer require magento/magento2-base $VERSION --no-update
